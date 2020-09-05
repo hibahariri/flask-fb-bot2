47151d6994cd6640fe74c760f5e670a9abe76bfa
hhariri <hariri_hiba@hotmail.com>
2020-09-05 20:45:12 +0300

    connect to mysql db 26

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ def receive_message():
                 if message.get('message'):
                     recipient_id = message['sender']['id']
                     print(recipient_id)
-                    r = requests.get(
-                        'https://graph.facebook.com/{}?fields=first_name,last_name,profile_pic&access_token={}'.format(
-                            recipient_id, ACCESS_TOKEN)).json()
-                    f_name = r['first_name']
-                    print(f_name)
-                    l_name = r['last_name']
+                    #  r = requests.get(
+                    #     'https://graph.facebook.com/{}?fields=first_name,last_name,profile_pic&access_token={}'.format(
+                    #          recipient_id, ACCESS_TOKEN)).json()
+                    #  f_name = r['first_name']
+                    #  print(f_name)
+                    # l_name = r['last_name']
                     if message['message'].get('text'):
                         messaging_text = message['message']['text']  # take message
                         print(messaging_text)
