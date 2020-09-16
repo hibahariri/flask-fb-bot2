9fe75092afbc4af174653e04505890e44617acaa
hhariri <hariri_hiba@hotmail.com>
2020-09-16 23:00:35 +0300

    Delete Ice breakers from welcome screen 6

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ data4 = {
 # grtreq = requests.post(fb_url, headers=headers, data=json.dumps(data2)).json()
 # del_icbr = requests.delete(fb_url, headers=headers, data=json.dumps(data3)).json()
 per_req = requests.post(fb_url, headers=headers, data=json.dumps(data4)).json()
-# print(del_icbr)
+print(per_req)
 
 
 @app.route('/', methods=['GET', 'POST'])
