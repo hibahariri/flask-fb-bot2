d8fc909dc00a4541790800929fcbc3a7193a7a9f
hhariri <hariri_hiba@hotmail.com>
2020-09-18 15:13:13 +0300

    Get payload from dialogflow 4

--- app.py
+++ app.py
@@ -81,11 +81,9 @@ def get_message(message_sent):
     payload = result.get('payload')
     if json_response['result']['fulfillment']['speech']:
         user_response = json_response['result']['fulfillment']['speech']
-    elif payload is not None:
-        print(payload)
-        user_response = "I didnt understand"
     else:
         user_response = "There is no response"
+    print(payload)
     return user_response
 
 
