1a68250bc39b71a3114ebbc60a9ef36e731e14cd
hhariri <hariri_hiba@hotmail.com>
2020-10-12 14:51:19 +0300

    products 19

--- DatabaseResponse.py
+++ DatabaseResponse.py
@@ -44,9 +44,8 @@ def get_products(subcatID):
     con = connect_todb()
     cur = con[0].cursor()
     cur.execute(
-        "Select productsname from product inner join subcategory where product.subcatid = subcategory.subcatID and subcatName = 'Accessories & Toys'")
-    #           "%s",
-    #           (subcatID,))
+        "Select productsname from product inner join subcategory where product.subcatid = subcategory.subcatID and subcatName = '%s'",
+        (subcatID,))
     print(subcatID)
     records = cur.fetchall()
     print(records)
