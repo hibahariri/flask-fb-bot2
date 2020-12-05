d7ff4458249cdcee6859f79008fed120b23ce3f9
hhariri <hariri_hiba@hotmail.com>
2020-12-05 23:16:58 +0200

    Get Started 7

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def send_message(recipient_id, response):
         quick_replies = []
         records = response[0]
         for row in records:
-            quick_replies.append({"content_type": "text", "title": row[0], "payload": "Help", })
+            quick_replies.append({"content_type": "text", "title": row[0], "payload": "Department", })
         bot.send_message(recipient_id, {
             "text": "Pick a category:",
             "quick_replies": quick_replies
