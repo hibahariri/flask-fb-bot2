2bfcc26f20fb0cbc0ed75d86bd2f60f743766b14
hhariri <hariri_hiba@hotmail.com>
2021-01-12 13:47:31 +0200

    Ordered successfully 1

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def fillAddress(recid, rec):
     else:
         adr = [request.form['Fullname'], request.form['Address1'], request.form['Address2'], request.form['Phone']]
         ret = DatabaseResponse.fill_Address(recid, adr, rec)
-        filename = os.path.join(app.config['Images', 'Deliveryscooter.png'])
+        filename = os.path.join(app.config['Images'], 'Deliveryscooter.png')
         return render_template('OrderConfirmed.html',filename=filename)
 
 
